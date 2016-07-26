d46e93ed0bbd679d79b06e57e07764d1d6063006
tracer-ics2015 <tracer@njuics.org>
2016-07-26 21:31:28 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  21:31:28 up 1 day,  4:20,  2 users,  load average: 0.03, 0.04, 0.05 7e8324608d9bf6468e6f5d151c106572a0e92671

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -30,7 +30,7 @@ static struct rule {
     {"\\/", '/'},					// by/divide
     {"\\(", '('},					// 
     {"\\)", ')'},					// 
-    {"\\$e[(a|b)x|(s|b)p]", REG},		// register
+    {"\\$e[(ax|bx)|(sp|bp)]", REG},		// register
     {"0x[0-9a-f]+", HEX},				// hexadecimal-number
     {"[0-9]+", NUM},					// decimal-number
     {"==", EQ},						// equal
