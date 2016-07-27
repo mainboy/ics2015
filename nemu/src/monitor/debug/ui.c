63ffee2a9f082eb7e3a6e23e13dafbe489201b51
tracer-ics2015 <tracer@njuics.org>
2016-07-27 11:08:36 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  11:08:36 up 1 day,  8:36,  2 users,  load average: 0.04, 0.06, 0.05 104e1fbd4ef912a8c48c58629d9960e53c9b7691

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -13,150 +13,155 @@ void cpu_exec(uint32_t);
 
 /* We use the ``readline'' library to provide more flexibility to read from stdin. */
 char* rl_gets() {
-    static char *line_read = NULL;
+	static char *line_read = NULL;
 
-    if (line_read) {
-	free(line_read);
-	line_read = NULL;
-    }
+	if (line_read) {
+		free(line_read);
+		line_read = NULL;
+	}
 
-    line_read = readline("(nemu) ");
+	line_read = readline("(nemu) ");
 
-    if (line_read && *line_read) {
-	add_history(line_read);
-    }
+	if (line_read && *line_read) {
+		add_history(line_read);
+	}
 
-    return line_read;
+	return line_read;
 }
 
 static int cmd_c(char *args) {
-    cpu_exec(-1);
-    return 0;
+	cpu_exec(-1);
+	return 0;
 }
 
 static int cmd_q(char *args) {
-    return -1;
+	return -1;
 }
 
 static int cmd_help(char *args);
 
 static int cmd_si(char *args) {
-    cpu_exec(NULL==args?1:atoi(args));
-    return 0;
+	cpu_exec(NULL==args?1:atoi(args));
+	return 0;
 }
 
 static int cmd_info(char *args) {
-    if (strcmp(args, "r")==0 || strcmp(args, "reg")==0){
-	printf("eax	0x%x\n",cpu.eax);
-	printf("ecx	0x%x\n",cpu.ecx);
-	printf("edx	0x%x\n",cpu.edx);
-	printf("ebx	0x%x\n",cpu.ebx);
-	printf("esp	0x%x\n",cpu.esp);
-	printf("ebp	0x%x\n",cpu.ebp);
-	printf("esi	0x%x\n",cpu.esi);
-	printf("edi	0x%x\n",cpu.edi);
-    }
+	if (strcmp(args, "r")==0 || strcmp(args, "reg")==0){
+		printf("eax	0x%x\n",cpu.eax);
+		printf("ecx	0x%x\n",cpu.ecx);
+		printf("edx	0x%x\n",cpu.edx);
+		printf("ebx	0x%x\n",cpu.ebx);
+		printf("esp	0x%x\n",cpu.esp);
+		printf("ebp	0x%x\n",cpu.ebp);
+		printf("esi	0x%x\n",cpu.esi);
+		printf("edi	0x%x\n",cpu.edi);
+	}
 
-    return 0;
+	return 0;
 }
 
 static int cmd_x(char *args) {
-    char* num = strtok(args, " ");
-    args = num + strlen(num) + 1;
-    swaddr_t addr;
-    sscanf(args, "%x\n",&addr);
-    int len = atoi(num),i;
-    printf("%8x:    ", addr);
-    for (i=0; i<len; i++){
-	printf("%02x ",instr_fetch(addr, 1));
-	addr = addr+1;
-    }
-    printf("\n");
-    return 0;
+	char* num = strtok(args, " ");
+	args = num + strlen(num) + 1;
+	swaddr_t addr;
+	sscanf(args, "%x\n",&addr);
+	int len = atoi(num),i;
+	printf("%8x:    ", addr);
+	for (i=0; i<len; i++){
+		printf("%02x ",instr_fetch(addr, 1));
+		addr = addr+1;
+	}
+	printf("\n");
+	return 0;
 }
 
 static int cmd_p(char *args) {
-    bool success=true;
-    int val = expr(args, &success);
-    if (success == false)
-	panic("Some errors happened.");
-    printf("%d	    0x%x\n",val, val);
-    return 0;
+	bool success=true;
+	int val = expr(args, &success);
+	if (success == false)
+		panic("Some errors happened.");
+	printf("%d	    0x%x\n",val, val);
+	return 0;
+}
+
+static int cmd_w(char *args) {
+	
+	return 0;
 }
 
 static struct {
-    char *name;
-    char *description;
-    int (*handler) (char *);
+	char *name;
+	char *description;
+	int (*handler) (char *);
 } cmd_table [] = {
-    { "help", "Display informations about all supported commands", cmd_help },
-    { "c", "Continue the execution of the program", cmd_c },
-    { "q", "Exit NEMU", cmd_q },
-    { "si", "Step once or more", cmd_si },
-    { "info", "Print state of registers or infomation of watchpoints", cmd_info },
-    { "x", "Scan instructions in memory", cmd_x },
-    { "p", "Test expr", cmd_p },
+	{ "help", "Display informations about all supported commands", cmd_help },
+	{ "c", "Continue the execution of the program", cmd_c },
+	{ "q", "Exit NEMU", cmd_q },
+	{ "si", "Step once or more", cmd_si },
+	{ "info", "Print state of registers or infomation of watchpoints", cmd_info },
+	{ "x", "Scan instructions in memory", cmd_x },
+	{ "p", "Test expr", cmd_p },
+	{ "w", "Implement watchpoints", cmd_w },
 
-
-    /* TODO: Add more commands */
+	/* TODO: Add more commands */
 
 };
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
 
 static int cmd_help(char *args) {
-    /* extract the first argument */
-    char *arg = strtok(NULL, " ");
-    int i;
+	/* extract the first argument */
+	char *arg = strtok(NULL, " ");
+	int i;
 
-    if(arg == NULL) {
-	/* no argument given */
-	for(i = 0; i < NR_CMD; i ++) {
-	    printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+	if(arg == NULL) {
+		/* no argument given */
+		for(i = 0; i < NR_CMD; i ++) {
+			printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+		}
 	}
-    }
-    else {
-	for(i = 0; i < NR_CMD; i ++) {
-	    if(strcmp(arg, cmd_table[i].name) == 0) {
-		printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-		return 0;
-	    }
+	else {
+		for(i = 0; i < NR_CMD; i ++) {
+			if(strcmp(arg, cmd_table[i].name) == 0) {
+				printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+				return 0;
+			}
+		}
+		printf("Unknown command '%s'\n", arg);
 	}
-	printf("Unknown command '%s'\n", arg);
-    }
-    return 0;
+	return 0;
 }
 
 void ui_mainloop() {
-    while(1) {
-	char *str = rl_gets();
-	char *str_end = str + strlen(str);
+	while(1) {
+		char *str = rl_gets();
+		char *str_end = str + strlen(str);
 
-	/* extract the first token as the command */
-	char *cmd = strtok(str, " ");
-	if(cmd == NULL) { continue; }
+		/* extract the first token as the command */
+		char *cmd = strtok(str, " ");
+		if(cmd == NULL) { continue; }
 
-	/* treat the remaining string as the arguments,
-	 * which may need further parsing
-	 */
-	char *args = cmd + strlen(cmd) + 1;
-	if(args >= str_end) {
-	    args = NULL;
-	}
+		/* treat the remaining string as the arguments,
+		 * which may need further parsing
+		 */
+		char *args = cmd + strlen(cmd) + 1;
+		if(args >= str_end) {
+			args = NULL;
+		}
 
 #ifdef HAS_DEVICE
-	extern void sdl_clear_event_queue(void);
-	sdl_clear_event_queue();
+		extern void sdl_clear_event_queue(void);
+		sdl_clear_event_queue();
 #endif
 
-	int i;
-	for(i = 0; i < NR_CMD; i ++) {
-	    if(strcmp(cmd, cmd_table[i].name) == 0) {
-		if(cmd_table[i].handler(args) < 0) { return; }
-		break;
-	    }
-	}
+		int i;
+		for(i = 0; i < NR_CMD; i ++) {
+			if(strcmp(cmd, cmd_table[i].name) == 0) {
+				if(cmd_table[i].handler(args) < 0) { return; }
+				break;
+			}
+		}
 
-	if(i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
-    }
+		if(i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+	}
 }
