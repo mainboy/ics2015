2a285372adb033cfd602718c79fae44e721e98da
tracer-ics2015 <tracer@njuics.org>
2016-08-01 12:49:53 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  12:49:53 up 3 days,  2:29,  2 users,  load average: 0.20, 0.13, 0.08 6ac92fdc67176f5c87b5bb71a0e809c3ba2f03cb

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -3,13 +3,14 @@
 #define instr sub
 
 static void do_execute() {
-	Log("op_src.imm = %x\n", op_src->simm);
+	//Log("op_src.imm = %x\n", op_src->simm);
 
 	panic("Please Implement me.");
 }
 
 make_instr_helper(i2a)
 make_instr_helper(i2rm)
+make_instr_helper(si2rm)
 make_instr_helper(r2rm)
 make_instr_helper(rm2r)
 
