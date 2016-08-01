9b1897df6d7e9f14bb016bb4c9933918531fef99
tracer-ics2015 <tracer@njuics.org>
2016-08-01 20:44:57 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  20:44:57 up 3 days,  6:25,  2 users,  load average: 0.18, 0.18, 0.11 8f07f8992c0fcaddbd5188d1f3b6a2e493a43b52

---
+++ nemu/src/cpu/exec/ctl-transfer/call-template.h
@@ -0,0 +1,13 @@
+#include "cpu/exec/template-start.h"
+
+#define instr call
+
+static void do_execute() {
+	panic("Please Implement me.");
+}
+
+
+make_instr_helper(i)
+
+
+#include "cpu/exec/template-end.h"
