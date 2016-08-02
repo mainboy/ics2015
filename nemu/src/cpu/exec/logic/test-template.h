f12de292a767a81bf1ddcdb6ca539f7b1f77cf36
tracer-ics2015 <tracer@njuics.org>
2016-08-02 15:18:20 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  15:18:20 up 3 days, 13:07,  2 users,  load average: 0.03, 0.03, 0.05 658ed888b6ca1ba2b375e33d2d18af591880bcfc

--- nemu/src/cpu/exec/logic/test-template.h
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -3,7 +3,7 @@
 #define instr test
 
 static void do_execute() {
-	Log("%s\n%x\n%x\n",REG_NAME(op_src->reg),REG(op_src->reg),REG(op_dest->reg));
+	Log("%s\n%x\n%x\n",REG_NAME(op_dest->reg),REG(op_src->reg),REG(op_dest->reg));
 	panic("Please implement me.");
 }
 
