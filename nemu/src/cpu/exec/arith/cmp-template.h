4ce98c862d488daa790e87ca67ca197e4c30c40c
tracer-ics2015 <tracer@njuics.org>
2016-08-02 16:53:36 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  16:53:36 up 3 days, 14:43,  2 users,  load average: 0.02, 0.05, 0.05 a6c47e4315692b3772b17fc97e165d4610c5d1d

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -4,7 +4,18 @@
 
 static void do_execute() {
 	Log("%s\n%x\n",REG_NAME(op_dest->reg),op_src->val);
-	panic("Please Implement me.");
+	if (REG(op_dest->reg)-op_src->val==0) {
+		cpu.EFLAGS.ZF = 1;
+	} else {
+		cpu.EFLAGS.ZF = 0;
+		if (REG(op_dest->reg)-op_src->val<0) {
+			cpu.EFLAGS.CF = 1;
+		}else{
+			cpu.EFLAGS.CF = 0;
+		}
+	}
+
+	print_asm_template2();
 }
 
 make_instr_helper(si2rm)
