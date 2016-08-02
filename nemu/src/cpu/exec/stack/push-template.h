35c3ae17feba07afd6779162633cac09ced25f81
tracer-ics2015 <tracer@njuics.org>
2016-08-02 12:18:19 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  12:18:19 up 3 days, 12:27,  2 users,  load average: 0.08, 0.03, 0.05 4b06bc66afa9b884f91f1d7553ce794d347c1dbe

--- nemu/src/cpu/exec/stack/push-template.h
+++ nemu/src/cpu/exec/stack/push-template.h
@@ -10,8 +10,10 @@ static void do_execute() {
 		cpu.esp = cpu.esp - 4;
 		MEM_W(cpu.ss.val+cpu.esp, REG(op_src->reg));
 	}
-
+	swaddr_t data = MEM_R(cpu.ss.val+cpu.esp);
+	Log("%x\n%x\n",cpu.ss.val+cpu.esp,data);
 	print_asm_template1();
+	panic("hello world");
 }
 
 make_instr_helper(r)
