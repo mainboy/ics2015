be16347fc7b2b114d1cd118ce8cbab9927d6a4a6
tracer-ics2015 <tracer@njuics.org>
2016-08-03 10:01:38 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  10:01:38 up 3 days, 20:21,  2 users,  load average: 0.09, 0.05, 0.06 9c1de736b78216183b15e5273b5135ea9ae3cb40

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -26,10 +26,10 @@ static void do_execute() {
 	print_asm_template2();
 }
 
-	make_instr_helper(i2a)
-	make_instr_helper(i2rm)
-	make_instr_helper(si2rm)
-	make_instr_helper(r2rm)
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(si2rm)
+make_instr_helper(r2rm)
 make_instr_helper(rm2r)
 
 #include "cpu/exec/template-end.h"
