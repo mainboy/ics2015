9c15519928b5e666512e4d52026e480337f56632
tracer-ics2015 <tracer@njuics.org>
2016-08-03 10:00:36 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  10:00:36 up 3 days, 20:20,  2 users,  load average: 0.00, 0.01, 0.05 7e852ebf1cabc926fcf1d32c6a5e6b26641cdd62

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -2,15 +2,34 @@
 
 #define instr sub
 
+#define mflags(tmp, des, src);\
+	cpu.EFLAGS.CF= des < src;\
+	cpu.EFLAGS.ZF= (tmp==0);\
+	cpu.EFLAGS.SF= (MSB((DATA_TYPE_S)tmp) == 1);\
+	cpu.EFLAGS.OF= (MSB(des)!=MSB(src)) && (MSB(tmp)==MSB(src));\
+	cpu.EFLAGS.PF= 1;\
+	while(tmp){\
+		cpu.EFLAGS.PF = !cpu.EFLAGS.PF;\
+		tmp = tmp & (tmp-1);\
+	}\
+
 static void do_execute() {
-	OPERAND_W(op_dest,REG(op_dest->reg)-op_src->val);
+	if (op_src->type == OP_TYPE_SIMM) {
+		OPERAND_W(op_dest, REG(op_dest->reg)-op_src->simm);
+		DATA_TYPE tmpd = REG(op_dest->reg) - op_src->simm;
+		mflags(tmpd, REG(op_dest->reg), op_src->simm);
+	} else {
+		OPERAND_W(op_dest,REG(op_dest->reg)-op_src->val);
+		DATA_TYPE tmpd = REG(op_dest->reg) - op_src->val;
+		mflags(tmpd, REG(op_dest->reg), op_src->val);
+	}
 	print_asm_template2();
 }
 
-make_instr_helper(i2a)
-make_instr_helper(i2rm)
-make_instr_helper(si2rm)
-make_instr_helper(r2rm)
+	make_instr_helper(i2a)
+	make_instr_helper(i2rm)
+	make_instr_helper(si2rm)
+	make_instr_helper(r2rm)
 make_instr_helper(rm2r)
 
 #include "cpu/exec/template-end.h"
