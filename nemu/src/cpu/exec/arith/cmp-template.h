9a1a3bd8032797aacc327401aa903f2fec03b69a
tracer-ics2015 <tracer@njuics.org>
2016-08-03 10:32:03 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  10:32:03 up 3 days, 20:52,  2 users,  load average: 0.12, 0.07, 0.06 247a657ca7ecdb1d237db908130eafe9cae5bedb

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -2,21 +2,33 @@
 
 #define instr cmp
 
+#define eflags(tmp, des, src);\
+	cpu.EFLAGS.CF= des < src;\
+	cpu.EFLAGS.ZF= (tmp==0);\
+	cpu.EFLAGS.SF= (MSB((DATA_TYPE_S)tmp) == 1);\
+	cpu.EFLAGS.OF= (MSB(des)!=MSB(src)) && (MSB(tmp)    ==MSB(src));\
+	cpu.EFLAGS.PF= 1;\
+while(tmp){\
+	cpu.EFLAGS.PF = !cpu.EFLAGS.PF;\
+	tmp = tmp & (tmp-1);\
+}\
+
 static void do_execute() {
-	if (REG(op_dest->reg)-op_src->val==0) {
-		cpu.EFLAGS.ZF = 1;
+	if (op_src->type == OP_TYPE_SIMM) {
+DATA_TYPE tmp = REG(op_dest->reg) - op_src->simm;
+eflags(tmp, REG(op_dest->reg), op_src->simm);
 	} else {
-		cpu.EFLAGS.ZF = 0;
-		if (REG(op_dest->reg)-op_src->val<0) {
-			cpu.EFLAGS.CF = 1;
-		}else{
-			cpu.EFLAGS.CF = 0;
-		}
+DATA_TYPE tmp = REG(op_dest->reg) - op_src->val;
+eflags(tmp, REG(op_dest->reg), op_src->val);
 	}
 
 	print_asm_template2();
 }
 
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
 make_instr_helper(si2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
 
 #include "cpu/exec/template-end.h"
