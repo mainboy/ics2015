96ee243c69f6b00cc721e18c7bd378f72538817f
tracer-ics2015 <tracer@njuics.org>
2016-08-03 20:39:37 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  20:39:37 up 4 days, 15 min,  2 users,  load average: 0.00, 0.02, 0.05 a62fc63d871f789fb22c7728d0dd00068721363a

---
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -0,0 +1,37 @@
+#include "cpu/exec/template-start.h"
+
+#define instr add
+
+#define eflags(tmp, des, src);\
+	cpu.EFLAGS.CF= (tmp < des || tmp < src);\
+	cpu.EFLAGS.ZF= (tmp==0);\
+	cpu.EFLAGS.SF= (MSB((DATA_TYPE_S)tmp) == 1);\
+	cpu.EFLAGS.OF= (MSB(des)==MSB(src)) && (MSB(tmp)!=MSB(src));\
+	cpu.EFLAGS.PF= 1;\
+	while(tmp){\
+		cpu.EFLAGS.PF = !cpu.EFLAGS.PF;\
+		tmp = tmp & (tmp-1);\
+	}\
+
+static void do_execute() {
+	if (op_src->type == OP_TYPE_SIMM) {
+		OPERAND_W(op_dest, op_dest->val + op_src->simm);
+		DATA_TYPE tmp = op_dest->val + op_src->simm;			
+		eflags(tmp, op_dest->val, op_src->simm);
+	} else {
+		OPERAND_W(op_dest, op_dest->val + op_src->val);
+		DATA_TYPE tmp = op_dest->val + op_src->val;
+		eflags(tmp, op_dest->val, op_src->val);   
+	}
+	print_asm_template2();
+
+	panic("Please");
+}
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(si2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#include "cpu/exec/template-end.h"
