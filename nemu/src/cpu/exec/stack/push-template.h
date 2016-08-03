4e22ce7ad4cc730821454ef0b55613b0a740df67
tracer-ics2015 <tracer@njuics.org>
2016-08-03 11:28:36 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  11:28:36 up 3 days, 21:48,  2 users,  load average: 0.09, 0.05, 0.05 c71045ab922f3c4666eca8504bbb9e1086167bbb

--- nemu/src/cpu/exec/stack/push-template.h
+++ nemu/src/cpu/exec/stack/push-template.h
@@ -3,17 +3,18 @@
 #define instr push
 
 static void do_execute() {
-		if (DATA_BYTE == 2) {
+	if (DATA_BYTE == 2) {
 		cpu.esp = cpu.esp - 2;
-		MEM_W(cpu.ss.val+cpu.esp, REG(op_src->reg));
+		MEM_W(cpu.ss.val+cpu.esp, op_src->val);
 	} else if (DATA_BYTE == 4) {
 		cpu.esp = cpu.esp - 4;
-		MEM_W(cpu.ss.val+cpu.esp, REG(op_src->reg));
+		MEM_W(cpu.ss.val+cpu.esp, op_src->val);
 	}
-
 	print_asm_template1();
 }
 
+make_instr_helper(rm)
 make_instr_helper(r)
+make_instr_helper(i)
 
 #include "cpu/exec/template-end.h"
