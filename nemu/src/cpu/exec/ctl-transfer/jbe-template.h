bff15b0b11658584662832353dbf8188cf8b3542
tracer-ics2015 <tracer@njuics.org>
2016-08-03 19:00:53 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  19:00:53 up 3 days, 23:34,  2 users,  load average: 0.17, 0.06, 0.06 595ef7ef911e9e4f1391cc3040c4a9b85dbfc0d7

--- nemu/src/cpu/exec/ctl-transfer/jbe-template.h
+++ nemu/src/cpu/exec/ctl-transfer/jbe-template.h
@@ -4,17 +4,13 @@
 
 static void do_execute() {
 	if(cpu.EFLAGS.CF==1 || cpu.EFLAGS.ZF==1) {
-		Log("%x\n",cpu.eip);
 		cpu.eip = cpu.eip+(DATA_TYPE_S)op_src->val;
-		Log("%x\n",cpu.eip);
 		if(DATA_BYTE == 2) {
 			cpu.eip = cpu.eip&0x0000ffff;
 		}
-		Log("%x\n",cpu.eip);
 	}
 
 	print_asm_template1();
-	panic("Please");
 }
 
 make_instr_helper(i)
