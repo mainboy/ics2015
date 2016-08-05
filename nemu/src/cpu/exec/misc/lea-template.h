938fc9580a966bbd454697acf113e8b8657c4fdd
tracer-ics2015 <tracer@njuics.org>
2016-08-05 16:51:27 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  16:51:27 up 4 days,  6:45,  2 users,  load average: 0.00, 0.03, 0.05 bb48ac5d8b4f868526b7b3a8bc75180ded6181b

--- nemu/src/cpu/exec/misc/lea-template.h
+++ nemu/src/cpu/exec/misc/lea-template.h
@@ -3,12 +3,8 @@
 #define instr lea
 
 static void do_execute() {
-	Log("%x\n%x\n",op_src->val, op_dest->val);
-	Log("%s eax %x\n",REG_NAME(op_dest->reg), cpu.eax);
 	
 	REG(op_dest->reg) = op_src->val;
-
-	panic("Please");
 }
 
 make_instr_helper(rm2r)
