7997569ef999ebc94aec3be4a263ed246f0a2bcb
tracer-ics2015 <tracer@njuics.org>
2016-08-05 14:06:41 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  14:06:41 up 4 days,  4:02,  2 users,  load average: 0.04, 0.16, 0.13 44e232d8eb894a0928012f44163eb0eed3b80b5

--- nemu/src/cpu/exec/data-mov/leave-template.h
+++ nemu/src/cpu/exec/data-mov/leave-template.h
@@ -7,7 +7,8 @@ static void do_execute() {
 	cpu.ebp = MEM_R(cpu.esp);
 	cpu.esp += DATA_BYTE;
 
-	print_asm_template1();
+	panic("hello world");
+	//print_asm_template1();
 }
 
 make_instr_helper(i)
