0c7a1aa23b32ac7b42d128d25710bc32ed7ff715
tracer-ics2015 <tracer@njuics.org>
2016-08-05 21:06:19 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  21:06:19 up 4 days,  8:17,  2 users,  load average: 0.08, 0.06, 0.05 4245aade98095015cea7f7649e3ca5012b42ca3e

--- nemu/src/cpu/exec/misc/lea-template.h
+++ nemu/src/cpu/exec/misc/lea-template.h
@@ -3,12 +3,12 @@
 #define instr lea
 
 static void do_execute() {
-	Log("%x\n",op_src->addr);
-	
 	if (op_src->size == DATA_BYTE)
 		op_dest->val = op_src->val;
 	else 
 		op_dest->val = (uint16_t)(op_src->val&0x0000ffff);
+
+	print_asm_template2();
 }
 
 make_instr_helper(rm2r)
