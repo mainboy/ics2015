7d6debb6b75779829b61730ef3f98cfb9e594412
tracer-ics2015 <tracer@njuics.org>
2016-08-06 23:23:43 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  23:23:43 up 4 days, 17:44,  2 users,  load average: 0.02, 0.06, 0.05 8da85d179d2bf23c532239a01464b1a8876977c5

--- nemu/src/cpu/exec/logic/xor-template.h
+++ nemu/src/cpu/exec/logic/xor-template.h
@@ -2,12 +2,23 @@
 
 #define instr xor
 
+#define eflags(tmp, des, src);\
+	cpu.EFLAGS.CF= 0;\
+        cpu.EFLAGS.ZF= (tmp==0);\
+        cpu.EFLAGS.SF= (MSB((DATA_TYPE_S)tmp) == 1);\
+        cpu.EFLAGS.OF= 0;\
+        cpu.EFLAGS.PF= 1;\
+        while(tmp){\
+		cpu.EFLAGS.PF = !cpu.EFLAGS.PF;\
+		tmp = tmp & (tmp-1);\
+	}\
+
 static void do_execute () {
 	DATA_TYPE result = op_dest->val ^ op_src->val;
 	OPERAND_W(op_dest, result);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	eflags(result, op_dest->val, op_src->val);
 
 	print_asm_template2();
 }
