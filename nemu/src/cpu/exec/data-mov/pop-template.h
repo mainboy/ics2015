ae64e3a5f5dbf09498de9a4d56748e173498ad56
tracer-ics2015 <tracer@njuics.org>
2016-08-06 23:16:21 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  23:16:21 up 4 days, 17:36,  2 users,  load average: 0.00, 0.04, 0.05 71b1143f6d6c720f7400bd59319289333c25f0bc

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -6,13 +6,9 @@ static void do_execute() {
 	REG(op_src->reg) = MEM_R(cpu.esp);
 	cpu.esp += DATA_BYTE;
 
-	Log("%s\n",REG_NAME(op_src->reg));
-	panic("hello");
 	print_asm_template1();
 }
 
-make_instr_helper(rm)
 make_instr_helper(r)
-make_instr_helper(i)
 
 #include "cpu/exec/template-end.h"
