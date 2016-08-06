885ac23ec890901f36f35acaeba211d6c00a89b1
tracer-ics2015 <tracer@njuics.org>
2016-08-06 09:54:45 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  09:54:45 up 4 days, 11:31,  2 users,  load average: 0.02, 0.06, 0.05 3a2d2308c751ca087393e05a1cb8add963a3c5

--- nemu/src/cpu/exec/ctl-transfer/ret-template.h
+++ nemu/src/cpu/exec/ctl-transfer/ret-template.h
@@ -2,22 +2,16 @@
 
 #define instr ret
 
-/*
-static void do_execute() {
+make_helper(concat(ret_, SUFFIX)) {
 	cpu.eip = MEM_R(cpu.esp);
 	cpu.esp += DATA_BYTE;
 	if (DATA_BYTE == 2) 
 		cpu.eip = cpu.eip & 0x0000ffff;
 
 	Log("eip=%x\n",cpu.eip);
-	cpu.eip -= DATA_BYTE;
-
 	print_asm_template1();
-}
-*/
+	cpu.eip -= 1;
 
-make_helper(concat(ret_, SUFFIX)) {
-	panic("hh");
 	return 1;
 }
 
