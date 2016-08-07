6bd81d18306a921578cce43382d5faee1057d4ca
tracer-ics2015 <tracer@njuics.org>
2016-08-07 18:45:45 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  18:45:45 up 4 days, 21:08,  2 users,  load average: 0.08, 0.03, 0.06 9e08eb3e6da3a54fc62fc75c71c4d50a3c3c20a7

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -5,8 +5,10 @@
 static void do_execute() {
 	
 	REG(op_dest->reg) = (DATA_TYPE_S)op_src->val;
+	Log("%x\n",op_src->size);
 	print_asm_template2();
-
+	
+	panic("Please implement me");
 }
 
 make_instr_helper(rm2r)
