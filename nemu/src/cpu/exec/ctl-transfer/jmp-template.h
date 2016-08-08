aebea8551eaace2026441a5086cfe5092c6ba8ef
tracer-ics2015 <tracer@njuics.org>
2016-08-08 09:32:10 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  09:32:10 up 5 days,  1:23,  2 users,  load average: 0.05, 0.08, 0.11 e6aa9332d1d80a3d643fb27837553bc8d22f20b0

--- nemu/src/cpu/exec/ctl-transfer/jmp-template.h
+++ nemu/src/cpu/exec/ctl-transfer/jmp-template.h
@@ -5,11 +5,12 @@
 static void do_execute() {
 	if (op_src->type == OP_TYPE_IMM)
 		cpu.eip = cpu.eip + op_src->val;
-	else
-		cpu.eip = MEM_R(op_src->val);
+	else {
+		cpu.eip = op_src->val;
+	}
 	
 	if (DATA_BYTE == 2) {
-		cpu.eip = cpu.eip&0x0000ffff;
+		cpu.eip &= 0x0000ffff;
 	}
 
 	print_asm_template1();
