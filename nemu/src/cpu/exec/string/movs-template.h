6c4e7410121e56902b720e14fdbf016ef3124b9a
tracer-ics2015 <tracer@njuics.org>
2016-08-08 08:25:34 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  08:25:34 up 5 days, 16 min,  2 users,  load average: 0.00, 0.02, 0.05 125e629895d24e7bc0e679a08b166acd9f8ff837

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -3,8 +3,16 @@
 #define instr movs
 
 make_helper(concat(movs_m_, SUFFIX)) {
-	
+	MEM_W(reg_l(R_EDI), MEM_R(reg_l(R_ESI)));
+	if (cpu.EFLAGS.DF) {
+		reg_l(R_EDI) -= DATA_BYTE;
+		reg_l(R_ESI) -= DATA_BYTE;
+	} else {
+		reg_l(R_EDI) += DATA_BYTE;
+		reg_l(R_ESI) += DATA_BYTE;
+	}
 
+	print_asm("movs");
 	return 1;
 }
 
