adb05e792e1b9b8f2f6efb143b7ff3a93af3b178
tracer-ics2015 <tracer@njuics.org>
2016-08-08 16:49:42 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  16:49:42 up 5 days,  4:48,  2 users,  load average: 0.06, 0.08, 0.06 83b033b26b171a43fdf72c6f2d00657acdd5c938

---
+++ nemu/src/cpu/exec/string/cmps-template.h
@@ -0,0 +1,35 @@
+#include "cpu/exec/template-start.h"
+
+#define instr cmps
+
+#define eflags(tmp, des, src);\
+	cpu.EFLAGS.CF= des < src;\
+        cpu.EFLAGS.ZF= (tmp==0);\
+        cpu.EFLAGS.SF= (MSB((DATA_TYPE_S)tmp) == 1);\
+        cpu.EFLAGS.OF= (MSB(des)!=MSB(src)) && (MSB(tmp)==MSB(src));\
+        cpu.EFLAGS.PF= 1;\
+	while(tmp){\
+		cpu.EFLAGS.PF = !cpu.EFLAGS.PF;\
+		tmp = tmp & (tmp-1);\
+	}
+
+
+make_helper(concat(cmps_m_, SUFFIX)) {
+	uint32_t src = MEM_R(reg_l(R_ESI));
+	uint32_t des = MEM_R(reg_l(R_EDI));
+	uint32_t result = src-des;
+
+	eflags(result,src,des);
+	if (cpu.EFLAGS.DF) {
+		reg_l(R_EDI) -= DATA_BYTE;
+		reg_l(R_ESI) -= DATA_BYTE;
+	} else {
+		reg_l(R_EDI) += DATA_BYTE;
+		reg_l(R_ESI) += DATA_BYTE;
+	}
+
+	print_asm("cmps");
+	return 1;
+}
+
+#include "cpu/exec/template-end.h"
