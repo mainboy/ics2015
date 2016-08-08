d87610076deb3d176a56632eb3d50cbc150427e7
tracer-ics2015 <tracer@njuics.org>
2016-08-08 10:01:54 +0800

    >  compile NEMU 141220000 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  10:01:54 up 5 days,  1:53,  2 users,  load average: 0.07, 0.10, 0.09 e45096dc34292c8962d33b167dddbfffd68a384

--- nemu/src/cpu/exec/ctl-transfer/jmp-template.h
+++ nemu/src/cpu/exec/ctl-transfer/jmp-template.h
@@ -2,28 +2,21 @@
 
 #define instr jmp
 
-make_helper(concat(jmp_i_, SUFFIX)) {
-	cpu.eip = cpu.eip + op_src->val;
+static void do_execute() {
+	if (op_src->type == OP_TYPE_IMM) {
+		cpu.eip = cpu.eip + op_src->val;
+	} else 
+		cpu.eip = op_src->val;
+
 	if (DATA_BYTE == 2) {
 		cpu.eip &= 0x0000ffff;
 	}
 
 	print_asm_template1();
-	cpu.eip -= 1;
 
-	return 1;
 }
 
-make_helper(concat(jmp_rm_, SUFFIX)) {
-	cpu.eip = op_src->val;
-	if (DATA_BYTE == 2) {
-		cpu.eip &= 0x0000ffff;
-	}
-
-	print_asm_template1();
-	cpu.eip -= 1;
-
-	return 1;
-}
+make_instr_helper(i)
+make_instr_helper(rm)
 
 #include "cpu/exec/template-end.h"
