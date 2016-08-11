c78524b7bcb013a3dfe0ea47d3f4ce6eeff85784
tracer-ics2015 <tracer@njuics.org>
2016-08-11 15:30:58 +0800

    >  compile NEMU U201215069 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  15:30:58 up 6 days,  7:31,  2 users,  load average: 0.12, 0.04, 0.05 581e63572de7c0f37c4d9e544e8a79d6b808ac0

--- nemu/src/cpu/exec/logic/shld-template.h
+++ nemu/src/cpu/exec/logic/shld-template.h
@@ -2,6 +2,15 @@
 
 #define instr shld
 
+#define eflags(tmp);\
+        cpu.EFLAGS.ZF= (tmp==0);\
+        cpu.EFLAGS.SF= (MSB((DATA_TYPE_S)tmp) == 1);\
+        cpu.EFLAGS.PF= 1;\
+        while(tmp){\
+		cpu.EFLAGS.PF = !cpu.EFLAGS.PF;\
+		tmp = tmp & (tmp-1);\
+	}
+
 #if DATA_BYTE == 2 || DATA_BYTE == 4
 static void do_execute () {
 	DATA_TYPE in = op_dest->val;
@@ -10,14 +19,15 @@ static void do_execute () {
 	uint8_t count = op_src->val;
 	count &= 0x1f;
 	while(count != 0) {
+		cpu.EFLAGS.CF = ((out >> ((DATA_BYTE << 3) - 1)) & 0x1); 
 		out <<= 1;
 		out |= ((in >> ((DATA_BYTE << 3) - 1)) & 0x1);
 		in <<= 1;
 		count --;
 	}
 
+	eflags(out);
 	OPERAND_W(op_src2, out);
-	cpu.EFLAGS.CF = 1;
 
 	print_asm("shld" str(SUFFIX) " %s,%s,%s", op_src->str, op_dest->str, op_src2->str);
 	
