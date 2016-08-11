2c151945c9eed70dcc416708796dc253ca9af2d7
tracer-ics2015 <tracer@njuics.org>
2016-08-11 22:12:35 +0800

    >  run U201215069 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  22:12:35 up 6 days,  9:12,  2 users,  load average: 0.01, 0.03, 0.05 b8ca22b513c616a9348effdd570206efa51a6de0

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -33,14 +33,30 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 }
 
 FLOAT f2F(float a) {
-	int d = *(int *)&a;
-	int s,e,m;
-	s = d >> 31;
-	e = ((d >> 23) & 0xff) - 127;
-	m = (d & 0x7fffff) | 0x800000;
-	if (s==0)
-		return m >> (7-e);
-	return -(m >> (7-e));
+	int i, uf, m, e, s, ans;
+	uf = *(int*)&a;
+	m = uf & ((1 << 23) - 1);
+	e = ((uf >> 23) & ((1 << 8) - 1)) - 127;
+	s = uf >> 31;
+	ans = 1;
+	for(i = 1; i <= e + 16; ++ i) {
+		ans = (ans << 1) + ((m & (1 << 22)) >> 22);
+		if (ans < 0) return 0x80000000u;
+		m = m << 1;
+	}
+	if (s != 0) ans = (~ans) + 1;
+	return (FLOAT)(ans);
+
+	/*
+	   int d = *(int *)&a;
+	   int s,e,m;
+	   s = d >> 31;
+	   e = ((d >> 23) & 0xff) - 127;
+	   m = (d & 0x7fffff) | 0x800000;
+	   if (s==0)
+	   return m >> (7-e);
+	   return -(m >> (7-e));
+	   */
 }
 
 FLOAT Fabs(FLOAT a) {
