f0b4bd1e4671baf082f94eca3720dd8ba19aec18
tracer-ics2015 <tracer@njuics.org>
2016-08-13 12:52:09 +0800

    >  compile NEMU U201215069 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  12:52:09 up 6 days, 15:20,  2 users,  load average: 0.15, 0.09, 0.06 653636c0e047a2f74f6280cb497361011cc8369

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -130,7 +130,7 @@ static int cmd_bt(char *args) {
 		name[31]= '\0';
 		printf("#%d  0x%08x  %s (",i++,addr,name);
 		for (index=2; index < 5; index++) {
-			printf("%x",swaddr_read(tmp+4*index,4));
+			printf("%x,",swaddr_read(tmp+4*index,4));
 		}
 		printf("%x",swaddr_read(tmp+4*index,4));
 		printf(")\n");
