29ae7fa2cf9bce4ab5142adb1acd540ffed2b133
tracer-ics2015 <tracer@njuics.org>
2016-08-15 15:24:59 +0800

    >  compile NEMU U201215069 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  15:24:59 up 6 days, 23:09,  2 users,  load average: 0.03, 0.04, 0.05 5b01185b2dcb40d5f9b085dbd72fa2ca3ced50f4

--- nemu/src/cpu/exec/misc/misc.c
+++ nemu/src/cpu/exec/misc/misc.c
@@ -30,7 +30,8 @@ make_helper(lgdt) {
 	int len = load_addr(eip + 1, &m, op_src);
 	cpu.gdtr.limit = lnaddr_read(op_src->addr, 2);
 	cpu.gdtr.base = lnaddr_read(op_src->addr+2, 4);
-	Log("lgdt seg_limit:0x%2x, base_addr:0x%x", cpu.gdtr.limit, cpu.gdtr.base);
+	Log("addr:0x%x addr+2:0x%x\n",op_src->addr, op_src->addr+2);
+	Log("lgdt seg_limit:0x%x, base_addr:0x%x", cpu.gdtr.limit, cpu.gdtr.base);
 
 	panic("Please Implement me");
 	return 1 + len;
