1831a95a27bd9f9e3efc729b1594e41666b8dfa0
tracer-ics2015 <tracer@njuics.org>
2016-08-16 09:29:12 +0800

    >  compile kernel U201215069 root Linux ubuntu 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:27 UTC 2016 i686 i686 i686 GNU/Linux  09:29:12 up 7 days,  2:51,  2 users,  load average: 0.07, 0.03, 0.05 4d387ae813129f1b4e6ee40778d16074a1069f5d

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -39,7 +39,6 @@ uint32_t loader() {
 	// panic("please implement me");
 	ph = (Elf32_Phdr *)(buf + elf->e_phoff);
 	uint16_t i=0;
-	set_bp();
 	for(i=0; i<elf->e_phnum; i++) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
@@ -54,7 +53,7 @@ uint32_t loader() {
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
 			memset((void *)vaddr+ph->p_filesz,0,ph->p_memsz-ph->p_filesz);
-
+			set_bp();
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
 			extern uint32_t brk;
